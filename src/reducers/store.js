448530037b57fe71a3cb4a81621fcd06485cb98c
santiabo17 <santiagoaboal17@gmail.com>
2024-05-19 00:49:06 -0300

    Cambios en el uso del LocalStorage, para permitir coexistencia con otras versiones del aplicativo

--- src/reducers/store.js
+++ src/reducers/store.js
@@ -1,8 +1,8 @@
 import {applyMiddleware, legacy_createStore as createStore} from 'redux';
 import { todosReducer } from './todos';
 
-const storageTodos = JSON.parse(localStorage.getItem('todos'));
-const tagsTodos = JSON.parse(localStorage.getItem('todos_tags'));
+const storageTodos = localStorage.getItem('todos_ls') != 'undefined' ? JSON.parse(localStorage.getItem('todos_ls')) : [];
+const tagsTodos = localStorage.getItem('todos_tags_ls') != 'undefined' ? JSON.parse(localStorage.getItem('todos_tags_ls')) : [];
 
 const initialState = {
     todos: storageTodos ? storageTodos : [['POR HACER', []], ['EN PROCESO', []], ['FINALIZADAS', []]],
@@ -20,8 +20,8 @@ store.subscribe(() => {
     const stringifiedTodos = JSON.stringify(todos);
     const tags = store.getState().tags;
     const stringifiedTags = JSON.stringify(tags);
-    localStorage.setItem('todos', stringifiedTodos);
-    localStorage.setItem('todos_tags', stringifiedTags);
+    localStorage.setItem('todos_ls', stringifiedTodos);
+    localStorage.setItem('todos_tags_ls', stringifiedTags);
 })
 
 export { store }
